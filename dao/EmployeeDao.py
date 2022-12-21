0cca660ce672e0911100e58883ba4ef669a47f97
simonebonifazi <simonbonifraz@gmail.com>
2022-12-21 11:47:46 +0100

    added employee crud

--- dao/EmployeeDao.py
+++ dao/EmployeeDao.py
@@ -1,6 +1,8 @@
 from dao.utility.db import MySql
 
+
 class EmployeeDao:
+    # read
     @classmethod
     def getAllEmployees(cls):
         MySql.openConnection()
@@ -9,7 +11,6 @@ class EmployeeDao:
         MySql.closeConnection()
         return results
 
-
     @classmethod
     def getEmployeeByNumber(cls, number):
         MySql.openConnection()
@@ -17,14 +18,50 @@ class EmployeeDao:
         results = MySql.getResults()
         MySql.closeConnection()
         return results
-        
-    
+
     @classmethod
     def getEmployeesByCity(cls, city):
         MySql.openConnection()
-        MySql.query(f"select * from employees INNER JOIN offices on employees.officeCode = offices.officeCode where offices.city = '{city}'")
+        MySql.query(
+            f"select * from employees INNER JOIN offices on employees.officeCode = offices.officeCode where offices.city = '{city}'")
         results = MySql.getResults()
         MySql.closeConnection()
         return results
 
+    # Create
 
+    @classmethod
+    def addEmployee(cls, employee_data):
+        MySql.openConnection()
+        MySql.query(
+            f"INSERT INTO employees\
+            VALUES(\
+                {employee_data['employeeNumber']},\
+                '{employee_data['lastName']}',\
+                '{employee_data['firstName']}',\
+                '{employee_data['extension']}',\
+                '{employee_data['email']}',\
+                {employee_data['officeCode']},\
+                {employee_data['reportsTo']},\
+                '{employee_data['jobTitle']}'\
+            )")
+        MySql.commit()
+        MySql.closeConnection()
+
+    # update
+    @classmethod
+    def updateEmployee(cls, employee_data):
+        MySql.openConnection()
+        MySql.query(
+            f"update employees set lastName =  '{employee_data['lastName']}', firstName =  '{employee_data['firstName']}' where employeeNumber = {employee_data['employeeNumber']}")
+        MySql.commit()
+        MySql.closeConnection()
+
+    # delete employee
+    @classmethod
+    def removeEmployee(cls, employee_number):
+        MySql.openConnection()
+        MySql.query(
+            f"delete from employees where employeeNumber = {employee_number}")
+        MySql.commit()
+        MySql.closeConnection()
