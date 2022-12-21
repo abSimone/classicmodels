079a5f326eb21751a42f31ca5c4c67a0e1e4d516
Dev1 - Samuel <samu_iori@hotmail.it>
2022-12-21 11:14:22 +0100

    add insert customer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # File che utilizzeremo in futuro :D
 
-from dao.gruppo3Dao import Gruppo3Dao
-result = Gruppo3Dao.getAllOrdersByEmployeeInfo()
-for res in result:
-    print(f"\n{res}")
+from dao.customersDao import CustomersDao
+CustomersDao.insertCustomer()
