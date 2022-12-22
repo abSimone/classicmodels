dd4ecb5fb86f24418377aac133cb582614d75004
dev1 - abba <leo.abbatelli@gmail.com>
2022-12-22 17:21:43 +0100

    payments dao aggiornato

--- dao/paymentsDao.py
+++ dao/paymentsDao.py
@@ -14,34 +14,7 @@ class PaymentsDao:
         return results
     
     @classmethod
-    def getAllCheckNumbers(cls):
-        MySql.openConnection()
-        MySql.query("SELECT checkNumber FROM payments")
-        data= MySql.getResults()
-        MySql.closeConnection()
-        
-        return data
-    
-    @classmethod
-    def getAllDates(cls):
-        MySql.openConnection()
-        MySql.query("SELECT paymentDate FROM payments")
-        data= MySql.getResults()
-        MySql.closeConnection()
-        
-        return data
-    
-    @classmethod
-    def getAllAmount(cls):
-        MySql.openConnection()
-        MySql.query("SELECT amount FROM payments")
-        data= MySql.getResults()
-        MySql.closeConnection()
-        
-        return data
-    
-    @classmethod
-    def getAllPaymentsCustomer(cls):
+    def getPaymentsNumberByCustomer(cls):
         customerNumber = int(input('Enter a customer number:\n'))
         MySql.openConnection()
         MySql.query(f"SELECT COUNT(*) FROM payments pay \
@@ -53,10 +26,7 @@ class PaymentsDao:
         return data
     
     @classmethod
-    def getAllDatePaymentsCountry(cls):
-        minDate = input('Enter a min date:\n')
-        maxDate = input('Enter a max date:\n')
-        country = input('Enter a country: \n')
+    def getAllDatePaymentsCountry(cls,minDate,maxDate,country):
         MySql.openConnection()
         MySql.query(f"SELECT * \
                     FROM payments pay \
