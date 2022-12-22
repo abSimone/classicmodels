52f787d769993adbc3204ed0edd51a63d1a471f0
Dev1 - bosco <abosco@visioture.com>
2022-12-22 10:48:51 +0100

    inserimento DTP per il customer

---
+++ dto/Customer.py
@@ -0,0 +1,9 @@
+class Customer:
+    def __init__(self, customernumber, name,lastname,firstname,phone,addressLine1):
+        self.customerNumber=customernumber
+        self.customerName=name
+        self.contactLastName=lastname
+        self.contactFirstName=firstname
+        self.phone=phone
+        self.addressLine1=addressLine1
+ 
