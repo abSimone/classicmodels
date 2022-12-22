73fb784e4f755495727e39dae4054f34548adfd7
MAugelli <m.augelli2@gmail.com>
2022-12-22 13:29:38 +0100

    aggiunta offices

--- dao/officesDao.py
+++ dao/officesDao.py
@@ -1,5 +1,4 @@
 from dao.utility.db import MySql
-from dto.Offices import Offices
 
 
 class OfficeDao:
@@ -11,21 +10,6 @@ class OfficeDao:
         results = MySql.getResults()
         MySql.closeConnection()
         return results
-    
-    @classmethod
-    def getOfficeByEmployeeNum(cls, employee_num):
-        MySql.openConnection()
-        MySql.query(f"SELECT o.officeCode, city, phone, addressLine1, addressLine2, state, country, postalCode, territory \
-                    FROM offices o \
-                    INNER JOIN employees e on o.officeCode = e.officeCode \
-                    WHERE e.employeeNumber=\"{employee_num}\"")
-        results = MySql.getResults()
-        data=list()
-        # for item in results:
-        data.append(Offices(results[0][0],results[0][1],results[0][2],results[0][3],results[0][4],results[0][5],results[0][6],results[0][7],results[0][8]))
-        MySql.closeConnection()
-        return data
-    
 
     # Create
     # office_data dev'essere un dict con le chiavi esplicitate per poter funzionare
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from dao.officesDao import OfficeDao
+from dao.customersDao import CustomersDao
 from dto.Customer import Customer
 
-result = OfficeDao.getOfficeByEmployeeNum(1002)
+result = CustomersDao.getAllCustomers()
 print(result)
 
+risultato_nomi = []
+for customer in result:
+    if len(customer.customerName) <= 6:
+        risultato_nomi.append([customer.contactFirstName, customer.contactLastName])
 
         
     
