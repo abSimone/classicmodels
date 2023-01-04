50307f8ddbf92d8ed57c014a21932dcf167b370c
Andrei - Stat <andreistatache1997@gmail.com>
2023-01-04 16:48:21 +0100

    name class order_details dao fixed

--- router/order_details.py
+++ router/order_details.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from dao.orderDetailDao import orderDetailsDao
+from dao.orderDetailDao import orderDetails
 
 from models.order_details import Order_details_model
 
@@ -8,7 +8,7 @@ router = APIRouter(prefix='/order_details', tags=['order_details'])
 
 @router.get('/all')
 async def getOrderDetails():
-    return {'oDetails' : orderDetailsDao.getAllOrdersDetails()}
+    return {'oDetails' : orderDetails.getAllOrdersDetails()}
 
 @router.post('/new', response_model = Order_details_model)
 async def addOrderDetail(order_detail : Order_details_model):
