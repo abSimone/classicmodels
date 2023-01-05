7e07f6804f2b7e4bbd1e52a0cba42541fe4f8f89
simonebonifazi <simonbonifraz@gmail.com>
2023-01-05 16:58:23 +0100

    ended test_main tests

--- router/order.py
+++ router/order.py
@@ -1,18 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from typing import List
 from dao.ordersDao import Orders
 from models.order import OrderModel
 
 router = APIRouter(prefix='/orders', tags=['orders'])
 
+
 @router.get(
     '/all',
-    response_model = List[OrderModel],
-    response_model_exclude_none = True,
-    response_model_include = {'orderNumber', 'orderDate', 'status', 'customerNumber'})
+    response_model=List[OrderModel],
+    response_model_exclude_none=True,
+    response_model_include={'orderNumber', 'orderDate', 'status', 'customerNumber'})
 async def getOffices():
     return Orders.getAllOrders()
 
-@router.post('/new', response_model = OrderModel, response_model_include={'orderNumber'})
-async def addOrder(order : OrderModel):
-  return order
+
+@router.post('/new', response_model=OrderModel, response_model_include={'orderNumber'}, status_code=status.HTTP_201_CREATED)
+async def addOrder(order: OrderModel):
+    return order
--- test_main.py
+++ test_main.py
@@ -3,6 +3,7 @@ from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
+
 def test_new_customer():
     response = client.post('/customer/new',
                            json={
@@ -22,4 +23,83 @@ def test_new_customer():
                            }
                            )
     assert response.status_code == 201
-    assert response.json() == {"customerNumber" : "0001"}
+    assert response.json() == {"customerNumber": "0001"}
+
+
+def test_get_customer():
+    response = client.get('/customer/all'
+                          )
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+
+
+# offices
+def test_new_office():
+    response = client.post('/offices/new',
+                           json={
+                               "officeCode": "5",
+                               "city": "philadephia",
+                               "phone": "355688597",
+                               "addressLine1": "via veneto 36",
+                               "addressLine2": "scala b int 8",
+                               "state": "italia",
+                               "country": "italia",
+                               "postalCode": "00000",
+                               "territory": "random",
+                           }
+                           )
+    assert response.status_code == 201
+    assert response.json() == {"officeCode": "5"}
+
+
+def test_get_office():
+    response = client.get('/offices/all'
+                          )
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+
+
+# orders
+def test_new_order():
+    response = client.post('/orders/new',
+                           json={
+                               "orderNumber": 500,
+                               "orderDate": 2023-1-23,
+                               "requiredDate": 2023-1-26,
+                               "shippedDate": 2023-1-27,
+                               "status": "processing",
+                               "comments": "",
+                               "customerNumber": 13,
+                           }
+                           )
+    assert response.status_code == 201
+    assert response.json() == {"orderNumber": 500}
+
+
+def test_get_orders():
+    response = client.get('/orders/all'
+                          )
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+
+
+# orders_detail
+def test_new_order_details():
+    response = client.post('/order_details/new',
+                           json={
+                               "orderNumber": 510,
+                               "productCode": "1321",
+                               "quantityOrdered": 24,
+                               "priceEach": 10.5,
+                               "orderLineNumber": 123,
+                           }
+                           )
+    assert response.status_code == 201
+    assert response.json() == {"productCode": "1321"}
+
+
+def test_get_order_details():
+    response = client.get('/order_details/all'
+                          )
+    assert response.status_code == 200
+    assert len(response.json()) > 0
