bbdc797533c3dbff292ea7a866de953f4111eac8
Ermes <ermesqwerty@gmail.com>
2023-01-05 15:49:12 +0100

    esercizio

--- test_main.py
+++ test_main.py
@@ -23,3 +23,39 @@ def test_new_customer():
                            )
     assert response.status_code == 201
     assert response.json() == {"customerNumber" : "0001"}
+
+def test_getEmployee():
+    response = client.get('/employee/all')
+    assert response.status_code==200
+    assert len(response.json())>0
+
+def test_new_employee():
+    response = client.post('/employee/new',
+                           json={
+                               "employeeNumber": "e0",
+                                "lastName": "a",
+                                "firstName": "b",
+                                "extension": "c",
+                                "email": "d",
+                                "officeCode": "d",
+                                "reportsTo": "e",
+                                "jobTitle": "f"
+                           }
+                           )
+    assert response.status_code==200
+
+def test_getProductLine():
+    response = client.get('/productLines/all')
+    assert response.status_code==200
+    assert len(response.json())>0
+
+def test_new_productline():
+    response = client.post('/productLines/new',
+                           json={
+                               "productLine": "pl1",
+                                "textDescription": "a",
+                                "htmlDescription": " ",
+                                "image": " "
+                           }
+                           )
+    assert response.status_code==200
